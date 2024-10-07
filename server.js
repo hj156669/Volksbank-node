e13cbeaef5890b4cc4bb43c3e39055f5bea96750
Johanna <156647086+hj156669@users.noreply.github.com>
2024-10-07 07:10:11 +0000

    ...

--- server.js
+++ server.js
@@ -44,4 +44,5 @@ app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
 // Mit der Funktion require wird die Datei in den runden Klammern eingelesen.
-require('./uebungen/03-objekte.js');
+//require('./uebungen/03-objekte.js');
+require('./klausuren/klausur20240930.js');
